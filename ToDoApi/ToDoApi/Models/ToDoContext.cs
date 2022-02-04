d135279003260e63e5598a20ce3dd47d49418333
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-02-03 22:18:30 -0500

    Create ToDoContext.cs

---
+++ ToDoApi/ToDoApi/Models/ToDoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using ToDoApi.Models;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> TodoItems { get; set; } = null!;
+    }
+}
